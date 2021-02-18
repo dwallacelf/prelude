193855ac2c168dc4818376ec4517aa3c67691ac7
Dave Wallace <dwallacelf@gmail.com>
2021-02-18 15:19:00 -0500

    Move beancount to /scratch/github.com
    - instead of having it be located in
      a /scratch/$USER directory.
    
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- personal/dwallacelf-init.el
+++ personal/dwallacelf-init.el
@@ -2,6 +2,6 @@
 ;; All rights reserved.
 
 ;; Beancount mode
-(add-to-list 'load-path "/scratch/dwallace/github.com/beancount/beancount-mode/")
+(add-to-list 'load-path "/scratch/github.com/beancount/beancount-mode/")
 (require 'beancount)
 (add-to-list 'auto-mode-alist '("\\.beancount\\'" . beancount-mode))
