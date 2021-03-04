25a57bb8bbf0086ae8fbe455b631f977b85ea481
Dave Wallace <dwallacelf@gmail.com>
2021-03-04 10:00:22 -0500

    Fix font height
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- personal/dwallacelf-init.el
+++ personal/dwallacelf-init.el
@@ -5,3 +5,6 @@
 (add-to-list 'load-path "/scratch/github.com/beancount/beancount-mode/")
 (require 'beancount)
 (add-to-list 'auto-mode-alist '("\\.beancount\\'" . beancount-mode))
+
+;; Set font height
+(set-face-attribute 'default nil :height 79)
